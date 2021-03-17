3ec739df87d4a1a22dbdd1ec87930036aeb48e96
pngwn <hello@pngwn.io>
2021-03-17 20:51:01 +0000

    test github contents api

--- .github/actions/publish/dist/main.js
+++ .github/actions/publish/dist/main.js
@@ -6002,12 +6002,32 @@ exports.getState = getState;
 
 var core$1 = /*@__PURE__*/getDefaultExportFromCjs(core);
 
+async function get_files_from_repo(client, target_repo, base_dir) {
+	const content = await client.repos.getContent({
+		owner: "pngwn",
+		repo: target_repo,
+		path: base_dir,
+	});
+
+	return await Promise.all(
+		content.data.map(async (data) => {
+			if (data.type === "file" && data.contents) return data;
+			else return await get_files_from_repo(client, target_repo, data.path);
+		})
+	);
+}
+
 function run() {
 	const type = core$1.getInput("type");
 	const target_repo = core$1.getInput("repo");
 	const base_dir = core$1.getInput("base");
+	const token = core$1.getInput("token");
 	console.log(type, "\n", target_repo, "\n", base_dir);
 	console.log(JSON.stringify(github$1, null, 2));
+
+	const octokit = github$1.getOctokit(token);
+	const files = get_files_from_repo(octokit, target_repo, base_dir);
+	console.log(files);
 
 	// get TAG, get PROJECT
 	// console.log(JSON.stringify(github.payload, null, 2));
--- .github/actions/publish/main.js
+++ .github/actions/publish/main.js
@@ -1,12 +1,32 @@
 import github from "@actions/github";
 import core from "@actions/core";
 
+async function get_files_from_repo(client, target_repo, base_dir) {
+	const content = await client.repos.getContent({
+		owner: "pngwn",
+		repo: target_repo,
+		path: base_dir,
+	});
+
+	return await Promise.all(
+		content.data.map(async (data) => {
+			if (data.type === "file" && data.contents) return data;
+			else return await get_files_from_repo(client, target_repo, data.path);
+		})
+	);
+}
+
 function run() {
 	const type = core.getInput("type");
 	const target_repo = core.getInput("repo");
 	const base_dir = core.getInput("base");
+	const token = core.getInput("token");
 	console.log(type, "\n", target_repo, "\n", base_dir);
 	console.log(JSON.stringify(github, null, 2));
+
+	const octokit = github.getOctokit(token);
+	const files = get_files_from_repo(octokit, target_repo, base_dir);
+	console.log(files);
 
 	// get TAG, get PROJECT
 	// console.log(JSON.stringify(github.payload, null, 2));
