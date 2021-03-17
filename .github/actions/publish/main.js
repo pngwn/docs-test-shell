896ea9d0db6251143879989c7c411d308faaa8d0
pngwn <hello@pngwn.io>
2021-03-17 00:30:50 +0000

    workflow stuff

--- .github/actions/publish/main.js
+++ .github/actions/publish/main.js
@@ -1,9 +1,30 @@
 import github from "@actions/github";
+import core from "@actions/core";
 
 function run() {
+	const type = core.getInput("type");
+	const docs = core.getInput("docs");
+	console.log(type, "\n", docs);
 	console.log(JSON.stringify(github, null, 2));
 
+	// get TAG, get PROJECT
 	// console.log(JSON.stringify(github.payload, null, 2));
+
+	// for each doc type -> tranform
+
+	// get VERSION from tag
+	// for each doctype: (DOCTYPE, DOCS) =>
+
+	// if (tag) {
+	// [DOCTYPE]:[PROJECT]:[VERSION] = [DOCS]
+	// [DOCTYPE]:[PROJECT]:latest = [DOCS]
+	// }
+
+	// [DOCTYPE]:[PROJECT]:next = [DOCS]
+
+	// write to disk => ${WORKFLOW_DIR}/.temp_kv_store
 }
 
 run();
+
+// What if there are multiple versions? Multiple tags?
